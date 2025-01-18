cca3a88d89c2eca2e31fbb418584be58b625d2a6
Mariano <marianonahuel2210@outlook.com>
2025-01-18 17:37:19 -0300

    SnackServices

--- src/MaquinaSnacks/Snack.java
+++ src/MaquinaSnacks/Snack.java
@@ -1,15 +1,25 @@
 package MaquinaSnacks;
+import java.io.Serializable;
+import java.util.Objects;
 
-public class Snack {
+public class Snack implements Serializable {
     private static int contadorSnacks;
     private final int idSnack;
     private String snackName;
     private double price;
 
-    public Snack(String snackName, double price) {
+    public Snack(){
         this.idSnack = ++Snack.contadorSnacks;
+    }
+
+    public Snack(String snackName, double price) {
+        this();
         this.snackName = snackName;
         this.price = price;
+    }
+
+    public static int getContadorSnacks() {
+        return contadorSnacks;
     }
 
     public int getIdSnack() {
@@ -32,10 +42,25 @@ public class Snack {
         this.price = price;
     }
 
-//    Puto el que lee
+    @Override
+    public String toString() {
+        return "Snack{" +
+                "idSnack=" + idSnack +
+                ", snackName='" + snackName + '\'' +
+                ", price=" + price +
+                '}';
+    }
 
     @Override
-    public String toString(){
-        return "Producto{" + "idProducto=" + this.idSnack + ", nombre=" + this.snackName + ", precio=" + this.price + "}";
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Snack snack = (Snack) o;
+        return idSnack == snack.idSnack && Double.compare(price, snack.price) == 0 && Objects.equals(snackName, snack.snackName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idSnack, snackName, price);
     }
 }
--- src/MaquinaSnacks/SnackServices.java
+++ src/MaquinaSnacks/SnackServices.java
@@ -1,4 +1,32 @@
 package MaquinaSnacks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SnackServices {
+    private static final List<Snack> snacks;
+
+    static{
+        snacks = new ArrayList<>();
+        snacks.add(new Snack("Papa", 70.0));
+        snacks.add(new Snack("Refresco", 50.0));
+        snacks.add(new Snack("Sandwich", 120.0));
+    }
+
+    public static void agregarSnack(Snack snack){
+        snacks.add(snack);
+    }
+
+    public static void mostrarSnacks(){
+        String inventarioSnack = "";
+        for (Snack snack:snacks){
+            inventarioSnack += snack.toString() + "\n";
+        }
+        System.out.println("--- Snacks en el Inventario ---");
+        System.out.println(inventarioSnack);
+    }
+
+    public static List<Snack> getSnacks(){
+        return snacks;
+    }
 }
