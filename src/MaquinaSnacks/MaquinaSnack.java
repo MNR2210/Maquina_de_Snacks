e0e23c96efe9351750f2a11ac74d4b53eb7d92bb
Mariano <marianonahuel2210@outlook.com>
2025-01-21 18:30:43 -0300

    arreglos de gitignore y creación de funciones

--- src/MaquinaSnacks/MaquinaSnack.java
+++ src/MaquinaSnacks/MaquinaSnack.java
@@ -19,7 +19,7 @@ public class MaquinaSnack {
         while (!salir){
             try{
                 int opcion = mostrarMenu(sc);
-                //salir = ejecutarOpciones(opcion, sc, productos);
+                salir = ejecutarOpciones(opcion, sc, productos);
             } catch (RuntimeException e) {
                 System.out.println("Ocurrió un error: " + e.getMessage());
             }
@@ -29,7 +29,7 @@ public class MaquinaSnack {
         }
     }
     private static int mostrarMenu(Scanner sc){
-        System.out.println("""
+        System.out.print("""
                 Menu:
                 1. Comprar snack
                 2. Mostrar ticket
@@ -37,5 +37,31 @@ public class MaquinaSnack {
                 4. Salir
                 Elige una opcion:\s""");
         return Integer.parseInt(sc.nextLine());
+    }
+
+    private static boolean ejecutarOpciones (int opcion, Scanner sc, List<Snack> productos){
+        boolean salir = false;
+        switch (opcion){
+            case 1 -> comprarSnack(sc, productos);
+//            case 2 -> mostrarTicket(sc, )
+        }
+        return salir;
+    }
+
+    private static void comprarSnack(Scanner sc, List <Snack> productos){
+        System.out.println("Qué querés comprar? Inserte el ID del producto: ");
+        int idSnack = Integer.parseInt(sc.nextLine());
+        boolean snackEncontrado = false;
+        for (Snack Snack : SnackServices.getSnacks()){
+            if (idSnack == Snack.getIdSnack()){
+                productos.add(Snack);
+                System.out.println("Producto Agregado con éxito. Producto: " + Snack);
+                snackEncontrado = true;
+                break;
+            }
+            if (!snackEncontrado){
+                System.out.println("Id de Snack no encontrado: " + idSnack);
+            }
+        }
     }
 }
